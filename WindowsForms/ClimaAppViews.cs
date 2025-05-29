211b4afc03b859c0044b043d8bea733a413a20b6
luci4585 <lucilencina6@gmail.com>
2025-05-29 14:15:36 -0300

    Añadir controles UI y funcionalidad para obtener clima
    Se han añadido nuevos controles a la interfaz de usuario, incluyendo un botón para obtener el clima y etiquetas para mostrar la temperatura. Se implementó el método `BtnObtenerClima_Click` para realizar solicitudes a una API meteorológica y actualizar la interfaz con los datos recibidos.
    
    Además, se han creado nuevas clases en el espacio de nombres `WindowsForms.Models` para manejar la estructura de los datos de la API. Se realizaron ajustes en el archivo de recursos para eliminar entradas redundantes y se añadió una nueva carpeta en el proyecto para mejorar la organización de los archivos.

--- WindowsForms/ClimaAppViews.cs
+++ WindowsForms/ClimaAppViews.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsForms.Models;
 using WindowsForms.Views;
 
 namespace WindowsForms
@@ -37,6 +39,20 @@ namespace WindowsForms
         private void subMenuNosotros_Click(object sender, EventArgs e)
         {
             botonNosotrosClick_Click(sender, e);
+        }
+
+        private async void BtnObtenerClima_Click(object sender, EventArgs e)
+        {
+            string url = "https://api.open-meteo.com/v1/forecast?latitude=-30.7891&longitude=-60.5919&current=temperature_2m,relative_humidity_2m,rain";
+
+            var clientHttp = new HttpClient();
+            var response = await clientHttp.GetAsync(url);
+            if (response != null)
+            {
+                QuertyTemperature quertyTemperature = await response.Content.ReadFromJsonAsync<QuertyTemperature>();
+                MessageBox.Show($"Temperatura actual es:" + quertyTemperature.current.temperature_2m);
+                this.LabelTemperatura.Text = $"Temperatura actual es: {quertyTemperature.current.temperature_2m} °C";
+            }
         }
     }
 }
---
+++ WindowsForms/Models/Current.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForms.Models
+{
+    //Clases que representan la información actual del clima
+    public class Current
+    {
+        public string time { get; set; }
+        public int interval { get; set; }
+        public double temperature_2m { get; set; }
+        public int relative_humidity_2m { get; set; }
+        public double rain { get; set; }
+    }
+
+}
