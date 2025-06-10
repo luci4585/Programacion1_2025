832197aea249bcb7f3b0458cbda2102f098220b2
luci4585 <lucilencina6@gmail.com>
2025-06-10 18:43:52 -0300

    Mejora de la interfaz y lógica en PeliculasView
    Se ha reestructurado la interfaz de usuario en `PeliculasView.Designer.cs`, reemplazando varios botones por un `TabControl` con dos `TabPage` para listar y agregar/editar películas. Se han añadido nuevos controles como `IconButton`, `TextBox`, `Label` y `NumericUpDown`.
    
    En `PeliculasView.cs`, se ha añadido una variable `url` para la API y se ha eliminado la declaración redundante en `ObtenemosPeliculas`. Se ha implementado un nuevo método `GridPeliculas_SelectionChanged_1` para mostrar la portada de la película seleccionada.
    
    Se han renombrado y mejorado los métodos de manejo de eventos para los botones de eliminar y agregar, y se ha implementado la funcionalidad para guardar películas utilizando `HttpClient`. Se han eliminado los botones `AgregarButton`, `ModificarButton` y `SalirButton`, logrando un diseño más limpio y organizado.

--- WindowsForms/PeliculasView.cs
+++ WindowsForms/PeliculasView.cs
@@ -15,6 +15,10 @@ namespace WindowsForms
     public partial class PeliculasView : Form
     {
         HttpClient clientHttp = new HttpClient();
+        string url = "https://cinesoftware-8275.restdb.io/rest/peliculas?apikey=510a2b7336a80665cf49419b623d4c4132ab0";
+        Pelicula peliculaModificada;
+
+
         public PeliculasView()
         {
             InitializeComponent();
@@ -23,7 +27,7 @@ namespace WindowsForms
 
         private async void ObtenemosPeliculas()
         {
-            string url = "https://cinesoftware-8275.restdb.io/rest/peliculas?apikey=510a2b7336a80665cf49419b623d4c4132ab0";
+
 
             var response = await clientHttp.GetAsync(url);
             if (response != null)
@@ -33,7 +37,17 @@ namespace WindowsForms
             }
         }
 
-        private async void BtnEliminar_Click(object sender, EventArgs e)
+
+        private void GridPeliculas_SelectionChanged_1(object sender, EventArgs e)
+        {
+            if (GridPeliculas.RowCount > 0 && GridPeliculas.SelectedRows.Count > 0)
+            {
+                Pelicula peliculaSeleccionada = (Pelicula)GridPeliculas.SelectedRows[0].DataBoundItem;
+                FilmPicture.ImageLocation = peliculaSeleccionada.portada;
+            }
+        }
+
+        private async void BtnEliminar_Click_1(object sender, EventArgs e)
         {
             //chequeamos que haya películas seleccionadas
             if (GridPeliculas.Rows.Count > 0 && GridPeliculas.SelectedRows.Count > 0)
@@ -58,12 +72,69 @@ namespace WindowsForms
             }
         }
 
-        private void GridPeliculas_SelectionChanged(object sender, EventArgs e)
+        private void BtnAgregar_Click(object sender, EventArgs e)
         {
-            if (GridPeliculas.RowCount > 0 && GridPeliculas.SelectedRows.Count >0)
+            LimpiarControlesAgregarEditar();
+            TabControl.SelectTab("TabpageAgregarEditar");
+        }
+        private void LimpiarControlesAgregarEditar()
+        {
+            TxtTitulo.Clear();
+            NumericDuracion.Value = 0;
+            TxtPortada.Clear();
+            NumericCalificacion.Value = 0;
+        }
+
+        private void BtnCancelar_Click(object sender, EventArgs e)
+        {
+            TabControl.SelectTab("TabPageLista");
+        }
+
+        private async void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            Pelicula peliculaAGuardar = new Pelicula
             {
-                Pelicula peliculaSeleccionada = (Pelicula)GridPeliculas.SelectedRows[0].DataBoundItem;
-                FilmPicture.ImageLocation = peliculaSeleccionada.portada;
+                titulo = TxtTitulo.Text,
+                duracion = (int)NumericDuracion.Value,
+                portada = TxtPortada.Text,
+                calificacion = (double)NumericCalificacion.Value
+            };
+
+            HttpResponseMessage response;
+            if (peliculaModificada != null)
+            {
+                var url = $"https://cinesoftware-8275.restdb.io/rest/peliculas/{peliculaModificada._id}?apikey=510a2b7336a80665cf49419b623d4c4132ab0";
+                response = await clientHttp.PutAsJsonAsync(url, peliculaAGuardar);
+            }
+            else
+            {
+                response = await clientHttp.PostAsJsonAsync(url, peliculaAGuardar);
+            }
+                if (response.IsSuccessStatusCode)
+                {
+                    peliculaModificada = null; // Reiniciamos la variable para futuras inserciones
+                    MessageBox.Show("Pelicula se guardó correctamente", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ObtenemosPeliculas();
+                    LimpiarControlesAgregarEditar();
+                    TabControl.SelectTab("TabPageLista");
+                }
+                else
+                {
+                    MessageBox.Show("Error al agregar la pelicula", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+        }
+
+        private void BtnModificar_Click(object sender, EventArgs e)
+        {
+            //chequeamos que haya películas seleccionadas
+            if (GridPeliculas.RowCount > 0 && GridPeliculas.SelectedRows.Count > 0)
+            {
+                peliculaModificada = (Pelicula)GridPeliculas.SelectedRows[0].DataBoundItem;
+                TxtTitulo.Text = peliculaModificada.titulo;
+                NumericDuracion.Value = peliculaModificada.duracion;
+                TxtPortada.Text = peliculaModificada.portada;
+                NumericCalificacion.Value = (decimal)peliculaModificada.calificacion;
+                TabControl.SelectTab("TabpageAgregarEditar");
             }
         }
     }
